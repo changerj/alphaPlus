de2d704155cdc7d916101b12eb1bc7be2d718a3f
ChangXu <297916496@qq.com>
2019-05-22 21:31:55 +0800

    项目初始化

---
+++ src/main/java/com/set/dao/User.java
@@ -0,0 +1,96 @@
+package com.set.dao;
+
+import java.util.Date;
+
+public class User {
+    private int user_id;
+    private int role_id;
+    private String user_name;
+    private String password;
+    private String telephone;
+    private int status;
+    private String nickname;
+    private int sex;
+    private String personal_sign;
+    private Date birthday;
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public int getRole_id() {
+        return role_id;
+    }
+
+    public void setRole_id(int role_id) {
+        this.role_id = role_id;
+    }
+
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public int getSex() {
+        return sex;
+    }
+
+    public void setSex(int sex) {
+        this.sex = sex;
+    }
+
+    public String getPersonal_sign() {
+        return personal_sign;
+    }
+
+    public void setPersonal_sign(String personal_sign) {
+        this.personal_sign = personal_sign;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+}
